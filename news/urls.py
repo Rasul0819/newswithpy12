cadad8365d4083ee197025c9d78c35208f75060d
Rasul0819 <rasulabdikerimov87@gmail.com>
2025-05-07 14:53:47 +0500

    category qosildi

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import homepage,detail
+from .views import homepage,detail,category_func
 
 
 urlpatterns =[
     path('',homepage,name='home'),
     path('news/<int:id>/',detail,name='detail'),
+    path('category/<int:id>/',category_func,name='category')
+
 ]
--- news/views.py
+++ news/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render,get_object_or_404
-from .models import News
+from .models import News,Category
 # Create your views here.
 
 
 
 def homepage(request):
     news = News.objects.all()
-    return render(request,'home.html',{'news':news})
-
+    categories = Category.objects.all()
+    return render(request,'home.html',{'news':news,
+                                       'categories':categories})
 
 def detail(request,id):
     news = get_object_or_404(News,id=id)
     return render(request,'detail.html',{'abc':news})
+
+
+def category_func(request,id):
+    categories = Category.objects.all()
+    news = News.objects.filter(category_id=id)
+
+    return render(request,'category.html',{'news':news,
+                                           'categories':categories})
